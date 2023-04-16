5785310a4b45586a34f0ffcbf2241d30a7d051dd
Seohyun Lee <studioseorii@gmail.com>
2023-04-16 12:30:22 +0900

    fix: apply http://stefnotch.github.io/web/COOP%20and%20COEP%20Service%20Worker/

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     import Monaco from "./Monaco.svelte";
     import Terminal from "./Terminal.svelte";
-    import {tick} from "svelte";
+    import {onMount, tick} from "svelte";
+    import {browser} from "$app/environment";
 
     let value, editor, run = false;
 
@@ -10,6 +11,22 @@
         await tick();
         run = true;
     }
+
+    if (browser) onMount(async () => {
+        if ("serviceWorker" in navigator) {
+            navigator.serviceWorker.register(new URL(location.href + "worker.js", import.meta.url)).then(
+                function (registration) {
+                    console.log("COOP/COEP Service Worker registered", registration.scope);
+                    if (registration.active && !navigator.serviceWorker.controller) window.location.reload();
+                },
+                function (err) {
+                    console.log("COOP/COEP Service Worker failed to register", err);
+                }
+            );
+        } else {
+            console.warn("Cannot register a service worker");
+        }
+    })
 </script>
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,5 @@ import {defineConfig} from 'vite';
 export default defineConfig({
     plugins: [
         sveltekit(),
-        {
-            name: "isolation",
-            configureServer(server) {
-                server.middlewares.use((_req, res, next) => {
-                    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-                    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-                    next();
-                });
-            },
-        },
     ]
 });
