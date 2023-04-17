3b4a71052e0827535a2bd7127f17649dba31b0c3
Seohyun Lee <studioseorii@gmail.com>
2023-04-17 20:25:08 +0900

    feat: enhance page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,29 @@
     import Monaco from "./Monaco.svelte";
     import Terminal from "$lib";
     import {page} from "$app/stores";
+    import {browser} from "$app/environment";
 
     let path = $page.url.pathname;
     if (path.endsWith('/')) path = path.slice(0, -1);
-    let value, editor, terminal, log = true, language = 'CPP';
+    let value, editor, terminal, log = true, language = 'CPP', init = false;
 
     async function exec() {
+        if (browser) {
+            localStorage.setItem('code', editor.getValue());
+            localStorage.setItem('language', language);
+        }
+        if (!('SharedArrayBuffer' in window)) location.reload();
         await terminal.clear();
         await terminal.prepare(language, editor.getValue(), log);
         await terminal.run(language, editor.getValue(), log);
+    }
+
+    $: if (browser && editor && !init) {
+        const code = localStorage.getItem('code');
+        const lang = localStorage.getItem('language');
+        if (code) editor.setValue(code);
+        if (lang) language = lang;
+        init = true;
     }
 </script>
 
