dcbf8e59207d441fb844bf409838361a68c9cc97
seo-rii <me@seorii.page>
2023-11-17 06:02:40 +0900

    fix: zip sysroot

--- src/lib/clang/url.ts
+++ src/lib/clang/url.ts
@@ -1,4 +1,4 @@
 export const memfsUrl = (prefix: string) => prefix + '/clang/bin/memfs.zip';
 export const clangUrl = (prefix: string) => prefix + '/clang/bin/clang.zip';
 export const lldUrl = (prefix: string) => prefix + '/clang/bin/lld.zip';
-export const rootUrl = (prefix: string) => prefix + '/clang/bin/sysroot.tar';
+export const rootUrl = (prefix: string) => prefix + '/clang/bin/sysroot.tar.zip';
--- src/lib/clang/wasm.ts
+++ src/lib/clang/wasm.ts
@@ -1,26 +1,25 @@
 import * as zip from "@zip.js/zip.js";
 
-const url = (url: string) => url
 const store: any = {}
+
+export const readBuffer = async (name: string) => {
+    const response = await fetch(name);
+    const blob = await response.blob();
+    const reader = new zip.ZipReader(new zip.BlobReader(blob));
+    const entries = await reader.getEntries();
+    for (const entry of entries) {
+        const data = await entry.getData(new zip.Uint8ArrayWriter());
+        return data;
+    }
+}
 
 export async function compile(filename: string) {
     // TODO: make compileStreaming work. It needs the server to use the
     // application/wasm mimetype.
     if (store[filename]) return store[filename];
-    const response = await fetch(url(filename));
-    const blob = await response.blob();
-    const reader = new zip.ZipReader(new zip.BlobReader(blob));
-    const entries = await reader.getEntries();
-    for (const entry of entries) {
-        console.log(`Found WASM file: ${entry.filename}`);
-        const data = await entry.getData(new zip.Uint8ArrayWriter());
-        return store[filename] = WebAssembly.compile(data);
-    }
+    return store[filename] = WebAssembly.compile(await readBuffer(filename));
 }
 
 export function getInstance(module: WebAssembly.Module, imports: WebAssembly.Imports) {
     return WebAssembly.instantiate(module, imports);
 }
-
-
-export const readBuffer = (name: string) => fetch(url(name)).then(r => r.arrayBuffer());
