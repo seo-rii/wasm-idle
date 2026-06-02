cd23425184fe4aeccd7817d90c71249b0468ea8f
seorii <me@seorii.page>
2026-06-02 20:02:05 +0900

    fix: instantiate Kotlin JVM class parts provider

--- tools/wasm-kotlin-teavm/src/main/java/org/wasmidle/kotlin/teavm/Patches.java
+++ tools/wasm-kotlin-teavm/src/main/java/org/wasmidle/kotlin/teavm/Patches.java
@@ -1841,6 +1841,12 @@ public final class Patches implements TeaVMPlugin, ClassHolderTransformer {
                                 .cast(languageSettingsInterfaceType))
                         .cast(objectType)
                         .returnValue());
+        pe.when(pe.var(0, descriptorType).getField("klass", classType)
+                .isSame(pe.constant(org.jetbrains.kotlin.resolve.jvm.JvmAdditionalClassPartsProvider.class)
+                        .cast(classType)))
+                .thenDo(() -> pe.construct("org.jetbrains.kotlin.resolve.jvm.JvmAdditionalClassPartsProvider")
+                        .cast(objectType)
+                        .returnValue());
         pe.var(0, descriptorType)
                 .invokeSpecial("createInstanceOf", objectType,
                         pe.var(0, descriptorType).getField("klass", classType),
